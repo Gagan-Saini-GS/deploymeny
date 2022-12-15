8e2940d602c66706bcfe38becfb006fa65581a65
Gagan Saini <gagansaini10999@gmail.com>
2022-12-15 18:12:57 +0530

    added static line in server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const jwt = require("jsonwebtoken");
 
 const app = express();
 app.use(express.json());
+app.use(express.static("public"));
 
 const DB =
   "mongodb+srv://Gagan_Saini:gaganiscoder@cluster0.afqpweg.mongodb.net/Impact?retryWrites=true&w=majority";
