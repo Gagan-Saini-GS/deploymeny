6b2ff1faae50ff0e9c297c4b45f6d8be3ea2ff7a
Gagan Saini <gagansaini10999@gmail.com>
2022-12-16 16:21:27 +0530

    removed comments

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 require("dotenv").config();
 
-// import { authUser } from "./middleware/basicAuth";
-
 const express = require("express");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
@@ -29,30 +27,11 @@ const userSchema = mongoose.Schema({
 
 const User = mongoose.model("newuser", userSchema);
 
-// app.get("/", (req, res) => {
-//   res.send("Hi");
-// });
-
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/build" + "/index.html");
 });
 
 app.get("/feed", (req, res) => {
-  //   const data = [
-  //     {
-  //       id: 1,
-  //       name: "Gagan Saini",
-  //     },
-  //     {
-  //       id: 2,
-  //       name: "Kira Ruke",
-  //     },
-  //     {
-  //       id: 3,
-  //       name: "Alan Walker",
-  //     },
-  //   ];
-
   User.find({}, function (err, foundUsers) {
     if (err) {
       console.log(err);
@@ -61,38 +40,6 @@ app.get("/feed", (req, res) => {
     }
   });
 });
-
-// app.post("/currentUser", (req, res) => {
-//   //   console.log(req.headers.accesstoken);
-//   const accessToken = req.headers.accesstoken;
-//   jwt.verify(accessToken, process.env.ACCESS_TOKEN, function (err, user) {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       res.json({ user });
-//     }
-//   });
-// });
-
-// app.post("/login", (req, res) => {
-//   const username = req.body.username;
-//   const useremail = req.body.useremail;
-
-//   const user = new User({
-//     username: username,
-//     useremail: useremail,
-//   });
-//   user.save();
-
-//   const accessToken = jwt.sign(
-//     {
-//       username: username,
-//       useremail: useremail,
-//     },
-//     process.env.ACCESS_TOKEN
-//   );
-//   res.json({ accessToken });
-// });
 
 const PORT = process.env.port || 5000;
 
